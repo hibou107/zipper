e3d07b37ae73d3bfd377898b646c9bbf64b0c724
Nam <nam.nguyen@the-ica.com>
2018-01-09 12:06:33 +0100

    init

---
+++ src/test/scala/com/github/hibou107/ZipperTests.scala
@@ -0,0 +1,15 @@
+package com.github.hibou107
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ZipperTests extends FlatSpec with Matchers {
+  it should "work" in {
+    val tree = Branch(Branch(Branch(Empty(), 1.0, Empty()), 3.0, Branch(Empty(), 2.0, Empty())), 1.0, Empty())
+    val zipper = Zipper.create(tree)
+    val bottom = zipper.goLeft().goLeft()
+    bottom.v shouldBe 1.0
+    val head = bottom.goUp().goUp()
+    head.v shouldBe 1.0
+  }
+
+}
